b1ad6570a988063d6a3fda1b2a1d382afeb64040
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-04-06 11:55:31 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
     @total 
   end
   
-  def add_item(title, price, quantity = nil)
+  def add_item(title, price, quantity = 1)
     @title = title 
     @price = price
     @quantity = quantity
